ad8e363463e646980623c6e1e37e979e01a72810
cellymin <1174064375@qq.com>
2018-12-26 17:23:12 +0800

    接口

--- api/Version1/Api/Goods/GetInfoByCode.php
+++ api/Version1/Api/Goods/GetInfoByCode.php
@@ -10,29 +10,35 @@ class Api_Goods_GetInfoByCode extends PhalApi_Api
         return array(
             'go' => array(
                 'goodsBarCode' => array('name' => 'goodsBarCode', 'type' => 'string', 'require' => true),
-                 'rate' => array('name' => 'rate', 'type' => 'float', 'require' => true),
+                 'rate' => array('name' => 'rate', 'type' => 'float', 'require' => false),
             )
         );
     }
 
     public function go()
     {
-        $rs = array('code'=>0,'content'=>'','msg'=>'');
+        $rs = array('code'=>0,'content'=>null,'msg'=>'');
         $domain = new Domain_Goods_CURD();
         if($this->rate>1){
-            $rs['msg'] = '税率不嗯呢该大于1';
+            $rs['msg'] = '税率不能大于1';
             return $rs;
         }
         $goods = $domain->getInfo($this->goodsBarCode);
-        if($goods['lastPrice']>$goods['usecostpri'] && $goods['usecostpri']>0){  //成本价>不含税价格
-            $goods['lastPrice'] = $goods['lastPrice']; //含税价
-        }else{
-            $goods['lastPrice'] = $goods['lastPrice'] * (1+$this->rate); //含税价格=不含税价*(1+税率)
-        }
         if(!$goods){
+
             $rs['msg'] = '商品不存在';
             return $rs;
         }
+        if(empty($goods['usecostpri'])){
+            if($this->rate>0) {
+                $goods['lastPrice'] = $goods['lastPrice'];
+            }else{
+                $goods['lastPrice'] = $goods['lastPrice'] * (1+$this->rate); //含税价格=不含税价*(1+税率)
+            }
+        }else if($goods['lastPrice']!=$goods['usecostpri']){
+            $goods['lastPrice'] = $goods['lastPrice'];
+        }
+
         $rs['code'] = 1;
         $rs['content'] = $goods;
 
