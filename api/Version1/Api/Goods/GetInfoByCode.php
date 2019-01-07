8054876d289ff70535cd4c306406b9d656d598f4
cellymin <1174064375@qq.com>
2019-01-07 18:51:51 +0800

    扫描商品含税价格调整显示

--- api/Version1/Api/Goods/GetInfoByCode.php
+++ api/Version1/Api/Goods/GetInfoByCode.php
@@ -5,39 +5,45 @@ class Api_Goods_GetInfoByCode extends PhalApi_Api
     protected function filterCheck()
     {
     }
+
     public function getRules()
     {
         return array(
             'go' => array(
                 'goodsBarCode' => array('name' => 'goodsBarCode', 'type' => 'string', 'require' => true),
-                 'rate' => array('name' => 'rate', 'type' => 'float', 'require' => false),
+                'rate' => array('name' => 'rate', 'type' => 'float', 'require' => false),
             )
         );
     }
 
     public function go()
     {
-        $rs = array('code'=>0,'content'=>null,'msg'=>'');
+        $rs = array('code' => 0, 'content' => null, 'msg' => '');
         $domain = new Domain_Goods_CURD();
-        if($this->rate>1){
+        if ($this->rate > 1) {
             $rs['msg'] = '税率不能大于1';
             return $rs;
         }
         $goods = $domain->getInfo($this->goodsBarCode);
-        if(!$goods){
+        if (!$goods) {
 
             $rs['msg'] = '商品不存在';
             return $rs;
         }
-        if(empty($goods['usecostpri'])){
-            if($this->rate>0) {
-                $goods['lastPrice'] = $goods['lastPrice'];
-            }else{
-                $goods['lastPrice'] = $goods['lastPrice'] * (1+$this->rate); //含税价格=不含税价*(1+税率)
-            }
-        }else if($goods['lastPrice']!=$goods['usecostpri']){
-            $goods['lastPrice'] = $goods['lastPrice'];
+        if (empty($this->rate)) {
+            $rs['msg'] = '供应商没有税率，请去后台维护';
+            return $rs;
         }
+        if (empty($goods['usecostpri'])) {
+
+            $goods['lastPrice'] = strval($goods['lastPrice'] * (1 + $this->rate)); //含税价格=不含税价*(1+税率)
+
+        } else if ($goods['lastPrice'] != round($goods['usecostpri'], 4)) {
+            $goods['lastPrice'] = $goods['lastPrice'];
+        } else if ($goods['lastPrice'] == round($goods['usecostpri'], 4)) {
+            $goods['lastPrice'] = strval($goods['usecostpri'] * (1 + $this->rate)); //含税价格=不含税价*(1+税率)
+        }
+
 
         $rs['code'] = 1;
         $rs['content'] = $goods;
