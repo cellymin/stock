5038b7aa918d815f6bfe87bc9fd40ee239c939bd
cellymin <1174064375@qq.com>
2019-03-19 14:11:10 +0800

    合并采购

--- api/Version1/Domain/Order/Save.php
+++ api/Version1/Domain/Order/Save.php
@@ -11,7 +11,7 @@ class Domain_Order_Save
 
     public function save($orderId, $reviewer,$flag='')
     {
-        if($this->type=='PURCHASE_IN' || $this->type=='USE_OUT' || $this->type=='ALLOT_OUT' || $this->type=='ALLOT_IN' || $this->type=='PURCHASE_RETURN'){
+        if($this->type=='PURCHASE_IN' || $this->type=='USE_OUT' || $this->type=='ALLOT_OUT' || $this->type=='ALLOT_IN' || $this->type=='USE_RETURN'|| $this->type=='PURCHASE_RETURN'){
             $flag=3;
         }
         $model = new Model_Order();
@@ -25,9 +25,11 @@ class Domain_Order_Save
         if ($order['totalCnt'] == 0) {
             throw new PhalApi_Exception_BadRequest('空订单不能提交！', 0);
         }
+
         //验证审核人
         $domain = new Domain_Reviewer_Options();
         $role = $domain->getRole($this->type);
+
         $user_model = new Model_User();
         $user_group = $user_model->getUserGroup($reviewer);
         $user_group_array = explode(',', $user_group[0]['group_role']);
@@ -35,6 +37,7 @@ class Domain_Order_Save
         if (!in_array($role, $user_group_array)) {
             throw new PhalApi_Exception_BadRequest('审核人没有权限！', 0);
         }
+
         if(empty($flag)){
             $input = array(
                 'flag'         => 1,
@@ -73,7 +76,9 @@ class Domain_Order_Save
             if (!$num) {
                 throw new PDOException('日志保存失败', 1);
             }
+
             //通知审核人
+
             $content = $this->toReviewerMsg($order['orderNo'], $order['totalMoney'], $order['createTime']);
             if (!Domain_Message_Msg::send($order['createUser'], $reviewer, $content, 3, $order['createCompany'])) {
                 throw new PDOException('通知失败', 1);
@@ -106,10 +111,25 @@ class Domain_Order_Save
                 }
                 //库存预警
                 Domain_Message_Msg::depotWarning($order['createCompany'], $reviewer);
+            }else if(in_array($this->type, array('USE_RETURN'))){
+                //商品退货
+                $input['depot_goods'] = $this->useReturn($orderId);
+                $log_model = new Model_LogDepot();
+                $num = $log_model->insert(array(
+                    'logUser'    => DI()->userInfo['userId'],
+                    'logType'    => 1,
+                    'logContent' => json_encode( $input['depot_goods']),
+                    'depotId'    => $order['depotId'],
+                    'orderId'    => $orderId,
+                    'orderType'  => $this->type,
+                    'createTime' => date('Y-m-d H:i:s')
+                ));
+                if (!$num) {
+                    throw new PDOException('库存日志错误', 1);
+                }
             }else if(in_array($this->type, array('PURCHASE_RETURN'))) {
                 //商品退货
                 $input['depot_goods'] = $this->ipReturn($orderId);
-
                 $log_model = new Model_LogDepot();
                 $num = $log_model->insert(array(
                     'logUser'    => DI()->userInfo['userId'],
@@ -297,7 +317,6 @@ class Domain_Order_Save
         }
         return $depot_input;
     }
-//采购退货出库
     protected function ipReturn($orderId)
     {
         $input = array();
@@ -307,7 +326,6 @@ class Domain_Order_Save
         if (!$goods) {
             throw new PDOException('订单商品不存在', 1);
         }
-
         $reGoods = array();
         $usingGoods = array();
         foreach ($goods as $k=>$v){
@@ -354,6 +372,66 @@ class Domain_Order_Save
         }
         return $input;
     }
+    protected function useReturn($orderId)
+    {
+        $input = array();
+        //订单商品
+        $goods_model = new Model_OrderGoods();
+        $goods = $goods_model->getAll($orderId);
+        if (!$goods) {
+            throw new PDOException('订单商品不存在', 1);
+        }
+        $reGoods = array();
+        $usingGoods = array();
+        foreach ($goods as $k=>$v){
+            $reGoods[$v['goodsId']][$v['orderSubNo']]['goodsCnt'] = $v['goodsCnt'];
+            $reids[] = $v['orderSubNo'];
+        }
+        //退货商品不存在于出库商品中
+
+        if (!$goods) {
+            throw new PDOException('订单商品不存在', 1);
+        }
+        $useGoods = $goods_model->getUseaGoods($orderId);
+        foreach ($useGoods as $kk=>$vv) {
+            $usingGoods[$vv['goodsId']][$vv['orderSubNo']]['goodsCnt'] = $vv['goodsCnt'];
+            if(isset($reGoods[$vv['goodsId']][$vv['orderSubNo']]  )){
+                if($usingGoods[$vv['goodsId']][$vv['orderSubNo']]['goodsCnt']<$reGoods[$vv['goodsId']][$vv['orderSubNo']]['goodsCnt']){
+                    throw new PDOException('商品数量大于出库数量', 1);
+                    break;
+                }
+            }
+            $usingids[] = $vv['orderSubNo'];
+        }
+        $diffids = array_diff($reids,$usingids);
+        if(!empty($diffids)){
+            throw new PDOException('商品不存在于出库清单中', 1);
+        }
+        //入库，更改库存
+        $depotGoods_model = new Model_DepotGoods();
+        foreach ($goods as $g) {
+            $depot_goods = $depotGoods_model->get($g['depotGoodsId']);
+            if (!$depot_goods || $depot_goods['flag'] != 1) {
+                throw new PDOException('库存商品不存在', 1);
+                break;
+            }
+            $goodsInfo_model = new Model_Goods();
+            $goodsinfo = $goodsInfo_model->fetch($depot_goods['goodsId']);
+            $num = DI()->notorm->depot_goods->where('id', $g['depotGoodsId'])->update(array('goodsCnt' => new NotORM_Literal("goodsCnt + $g[goodsCnt]")));
+            if ($num === false) {
+                throw new PDOException('库存更新失败', 1);
+                break;
+            }
+            $input[] = array(
+                'id'         => $g['depotGoodsId'],
+                'depotSubId' => $g['depotSubId'],
+                'batchNo'    => $depot_goods['batchNo'],
+                'goodsCnt'   => $g['goodsCnt']
+            );
+        }
+        return $input;
+    }
+
     /**
      * 商品出库
      * @return array
@@ -440,6 +518,7 @@ class Domain_Order_Save
             'INVENTORY'   => '盘点单',    //盘点单,
             'SALE_OUT'    => '销售单',
             'SALE_RETURN' => '销售退货单',
+            'USE_RETURN' => '领用退货单',
             'PURCHASE_RETURN' => '采购退货单'
         );
 
